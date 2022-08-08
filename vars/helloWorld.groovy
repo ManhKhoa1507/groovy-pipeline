1d2b492494a73f8e2c2e3d80ed96def0f2403eaf
khoanlm2 <khoanlm2@vng.com.vn>
2022-08-08 15:24:12 +0700

    Test

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -5,10 +5,6 @@ def logInfo(String message) {
     logger.info(message)
 }
 
-def logError(String message) { 
-
-}
-
 def call () {
     def workSpace = env.WORKSPACE
     def filePath = "${workSpace}/scripts/helloWorld.sh"
