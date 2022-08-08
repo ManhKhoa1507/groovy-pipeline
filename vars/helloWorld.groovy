61d8a22c8a568031654a713310cecb7d79487b39
khoanlm2 <khoanlm2@vng.com.vn>
2022-08-08 13:59:27 +0700

    Test

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,6 +1,6 @@
 def call () {
-    sh "pwd"
-    def filePath = "./scripts/helloWorld.sh"
+
+    def filePath = "/var/jenkins_home/workspace/pipeline/scripts/helloWorld.sh"
     def helloWorldScripts = readFile(filePath)
     echo helloWorldScripts
 }
